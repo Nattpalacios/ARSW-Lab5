295f3cfd535db559b9e8ee50697217bf04fdf121
Natalia Palacios <nataplc24@gmail.com>
2019-10-10 15:07:49 -0500

    Avance

--- BLUEPRINTS_PART1/BLUEPRINTS_PART1/src/main/resources/static/js/app.js
+++ BLUEPRINTS_PART1/BLUEPRINTS_PART1/src/main/resources/static/js/app.js
@@ -1,25 +1,17 @@
 var module = (function () {
   var authorName;
   var blueprints = {};
-  var mapearBlueprints = function (blueprint) {
-    console.log(blueprint);
+
+  var mapearBlueprints = function(blueprint) {
     authorName = blueprint[0].author;
     blueprints = blueprint;
-    var blueprintsMapeados = blueprints.map(function (blue){
-      var numeroPuntos = blue.points.length;
-      var nuevoBlueprint = new Object();
-      nuevoBlueprint.name = blue.name;
-      nuevoBlueprint.puntos = numeroPuntos;
-      return nuevoBlueprint;
+    return blueprints.map(function(blue) {
+      return { name: blue.name, points: blue.points.length };
     });
-    blueprints = blueprintsMapeados;
-    console.log(authorName);
-    console.log(blueprints);
-    return blueprintsMapeados;
   };
   return {
     cambiarNombreAutor: function (nuevoNombreAutor) {
-      this.authorName = nuevoNombreAutor;
+      authorName = nuevoNombreAutor;
     },
     actualizarBlueprints: function (author) {
       apimock.getBlueprintByAuthor(author,mapearBlueprints);
