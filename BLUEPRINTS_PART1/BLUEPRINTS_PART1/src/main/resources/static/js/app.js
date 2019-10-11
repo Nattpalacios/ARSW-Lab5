05594a3b3841a4b5c173e2189941ac2c3ab0573d
Natalia Palacios <nataplc24@gmail.com>
2019-10-11 18:22:43 -0500

    Avance

--- BLUEPRINTS_PART1/BLUEPRINTS_PART1/src/main/resources/static/js/app.js
+++ BLUEPRINTS_PART1/BLUEPRINTS_PART1/src/main/resources/static/js/app.js
@@ -3,23 +3,39 @@ var module = (function () {
   var blueprints = {};
 
   var mapearBlueprints = function(blueprint) {
+    
     authorName = blueprint[0].author;
+    
     blueprints = blueprint;
     bl = blueprints.map(function(blue) {
       return { name: blue.name, points: blue.points.length };
     });
+    blueprints = bl;
     return bl;
   };
+  var addRows = function(){
+    for(var i = 0; i < blueprints.length; i++){
+        var blueprint = blueprints[i];
+        var name = blueprint.name;
+        var numPoints = blueprint.points;
+        var txt = '<tr id="'+ (i + 1)+'">'+
+        '<th scope="row">'+name+'</th>'+
+        '<td>'+numPoints+'<td>';
+        $("#blueprints").append(txt);
+    }
+  }
+
   return {
     cambiarNombreAutor: function (nuevoNombreAutor) {
       authorName = nuevoNombreAutor;
       console.log(authorName);
     },
-    actualizarBlueprints: function (author) {
+    actualizarBlueprints: function () {
+      $('#blueprints').find('tbody').empty();
+      var author = $('#inputAuthor').val();
       apimock.getBlueprintByAuthor(author,mapearBlueprints);
+      this.agregarFilas();
     },
-    agregarFilas: function (author){
-      apimock.getBlueprintByAuthor(author,addRows);
-    }
+    agregarFilas: addRows
   };
 })();
